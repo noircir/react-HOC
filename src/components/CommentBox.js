2b6ea589a78828bdb6c2c9ccb6e8056fa3e7a206
Nellie K <noircir@users.noreply.github.com>
2019-01-05 15:57:29 -0500

    first commit

---
+++ src/components/CommentBox.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import * as actions from 'actions';
+
+class CommentBox extends React.Component {
+    state = {
+        comment: ''
+    }
+
+    commentInputHandler = (e) => {
+        this.setState({ comment: e.target.value })
+    }
+
+    formSubmitHandler = event => {
+        event.preventDefault();
+
+        // Call an action creator and save the comment
+        this.props.saveComment(this.state.comment);
+        this.setState({ comment: '' });
+    }
+
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.formSubmitHandler}>
+                    <h4>Add a Comment</h4>
+                    <textarea 
+                        value={this.state.comment}
+                        onChange={this.commentInputHandler} 
+                    />
+                    <div>
+                        <button>Submit Comment</button>
+                    </div>
+                </form>
+                <button 
+                    className="fetch-comments"
+                    onClick={this.props.fetchComments}
+                >
+                    Fetch Comments
+                </button>
+            </div>
+        )
+    }
+}
+
+export default connect(null, actions )(CommentBox);
